db7c0502400139ba7147b49312f00ea179640566
jpdriver <jpsdriver@gmail.com>
2016-04-16 00:38:32 +0100

    scaffold BasketsController

---
+++ BasketApp/Controllers/BasketsController.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using BasketApp.DAL;
+using BasketApp.Models;
+
+namespace BasketApp.Controllers
+{
+    public class BasketsController : Controller
+    {
+        private BasketAppContext db = new BasketAppContext();
+
+        // GET: Baskets
+        public ActionResult Index()
+        {
+            return View(db.Baskets.ToList());
+        }
+
+        // GET: Baskets/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Basket basket = db.Baskets.Find(id);
+            if (basket == null)
+            {
+                return HttpNotFound();
+            }
+            return View(basket);
+        }
+
+        // GET: Baskets/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Baskets/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "BasketID")] Basket basket)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Baskets.Add(basket);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(basket);
+        }
+
+        // GET: Baskets/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Basket basket = db.Baskets.Find(id);
+            if (basket == null)
+            {
+                return HttpNotFound();
+            }
+            return View(basket);
+        }
+
+        // POST: Baskets/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "BasketID")] Basket basket)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(basket).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(basket);
+        }
+
+        // GET: Baskets/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Basket basket = db.Baskets.Find(id);
+            if (basket == null)
+            {
+                return HttpNotFound();
+            }
+            return View(basket);
+        }
+
+        // POST: Baskets/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Basket basket = db.Baskets.Find(id);
+            db.Baskets.Remove(basket);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
