bed2833a3ee761e8a9a9484da446dd8a4c8b4a11
jpdriver <jpsdriver@gmail.com>
2016-04-16 00:37:17 +0100

    add DAL

---
+++ BasketApp/DAL/BasketAppContext.cs
@@ -0,0 +1,18 @@
+﻿using BasketApp.Models;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+
+namespace BasketApp.DAL
+{
+    public class BasketAppContext : DbContext
+    {
+        public DbSet<Item> Items { get; set; }
+        public DbSet<Basket> Baskets { get; set; }
+        public DbSet<BasketItem> BasketItems { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+    }
+}
---
+++ BasketApp/DAL/BasketAppInitializer.cs
@@ -0,0 +1,33 @@
+﻿using BasketApp.Models;
+using System.Collections.Generic;
+
+namespace BasketApp.DAL
+{
+    public class BasketAppInitializer : System.Data.Entity.DropCreateDatabaseIfModelChanges<BasketAppContext>
+    {
+        protected override void Seed(BasketAppContext context)
+        {
+            var items = new List<Item>
+            {
+                new Item { Product = "Butter", Price = 0.8 },
+                new Item { Product = "Milk", Price = 1.15 },
+                new Item { Product="Bread", Price = 1.0 }
+            };
+            items.ForEach(i => context.Items.Add(i));
+            context.SaveChanges();
+
+            var basket = new Basket();
+            context.Baskets.Add(basket);
+            context.SaveChanges();
+
+            var basketItems = new List<BasketItem>
+            {
+                new BasketItem { BasketID = 1, ItemID = 1 },
+                new BasketItem { BasketID = 1, ItemID = 2 },
+                new BasketItem { BasketID = 1, ItemID = 3 }
+            };
+            basketItems.ForEach(b => context.BasketItems.Add(b));
+            context.SaveChanges();
+        }
+    }
+}
