a537903aae55300eef74da4ced69ed16f519240a
jpdriver <jpsdriver@gmail.com>
2016-04-16 00:49:06 +0100

    change default route action name

--- BasketApp/App_Start/RouteConfig.cs
+++ BasketApp/App_Start/RouteConfig.cs
@@ -12,7 +12,7 @@ namespace BasketApp
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Baskets", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Baskets", action = "BasketList", id = UrlParameter.Optional }
             );
         }
     }
