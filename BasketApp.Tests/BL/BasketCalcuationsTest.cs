20a8af751732238a6e20edc74f8a23721457664a
jpdriver <jpsdriver@gmail.com>
2016-04-17 00:14:04 +0100

    add more milk offer tests

--- BasketApp.Tests/BL/BasketCalcuationsTest.cs
+++ BasketApp.Tests/BL/BasketCalcuationsTest.cs
@@ -25,6 +25,24 @@ namespace BasketApp.Tests.BL
         }
 
         [TestMethod]
+        public void Basic_No_Discounts()
+        {
+            var basket = new Basket { BasketItems = new List<BasketItem>() };
+            var items = new List<Item>
+            {
+                new Item { ItemID = 1, Product = "Butter", Price = 0.8 },
+                new Item { ItemID = 2, Product = "Milk", Price = 1.15 },
+                new Item { ItemID = 3, Product="Bread", Price = 1.0 }
+            };
+            foreach (var item in items)
+            {
+                basket.BasketItems.Add(new BasketItem { Item = item });
+            }
+            Assert.AreEqual(0.00, basket.Discounts);
+            Assert.AreEqual(basket.GrossTotal, basket.Total);
+        }
+
+        [TestMethod]
         public void Total_for_2_Bread_2_Butter()
         {
             var basket = new Basket { BasketItems = new List<BasketItem>() };
@@ -43,6 +61,41 @@ namespace BasketApp.Tests.BL
         }
 
         [TestMethod]
+        public void Total_for_3_Milk()
+        {
+            var basket = new Basket { BasketItems = new List<BasketItem>() };
+            var items = new List<Item>
+            {
+                new Item { ItemID = 2, Product = "Milk", Price = 1.15 },
+                new Item { ItemID = 2, Product = "Milk", Price = 1.15 },
+                new Item { ItemID = 2, Product = "Milk", Price = 1.15 }
+            };
+            foreach (var item in items)
+            {
+                basket.BasketItems.Add(new BasketItem { Item = item });
+            }
+            Assert.AreEqual(3.45, basket.Total);
+        }
+
+        [TestMethod]
+        public void Discount_for_3_Milk()
+        {
+            var basket = new Basket { BasketItems = new List<BasketItem>() };
+            var items = new List<Item>
+            {
+                new Item { ItemID = 2, Product = "Milk", Price = 1.15 },
+                new Item { ItemID = 2, Product = "Milk", Price = 1.15 },
+                new Item { ItemID = 2, Product = "Milk", Price = 1.15 }
+            };
+            foreach (var item in items)
+            {
+                basket.BasketItems.Add(new BasketItem { Item = item });
+            }
+            Assert.AreEqual(0.00, basket.Discounts);
+            Assert.AreEqual(basket.GrossTotal, basket.Total);
+        }
+
+        [TestMethod]
         public void Total_for_4_Milk()
         {
             var basket = new Basket { BasketItems = new List<BasketItem>() };
@@ -58,6 +111,114 @@ namespace BasketApp.Tests.BL
                 basket.BasketItems.Add(new BasketItem { Item = item });
             }
             Assert.AreEqual(3.45, basket.Total);
+        }
+
+        [TestMethod]
+        public void Discount_for_4_Milk()
+        {
+            var basket = new Basket { BasketItems = new List<BasketItem>() };
+            var items = new List<Item>
+            {
+                new Item { ItemID = 2, Product = "Milk", Price = 1.15 },
+                new Item { ItemID = 2, Product = "Milk", Price = 1.15 },
+                new Item { ItemID = 2, Product = "Milk", Price = 1.15 },
+                new Item { ItemID = 2, Product = "Milk", Price = 1.15 }
+            };
+            foreach (var item in items)
+            {
+                basket.BasketItems.Add(new BasketItem { Item = item });
+            }
+            Assert.AreEqual(1.15, basket.Discounts);
+        }
+
+        // check offer logic still works for totals with increased volumes
+        [TestMethod]
+        public void Total_for_7_Milk()
+        {
+            var basket = new Basket { BasketItems = new List<BasketItem>() };
+            var items = new List<Item>
+            {
+                new Item { ItemID = 2, Product = "Milk", Price = 1.15 },
+                new Item { ItemID = 2, Product = "Milk", Price = 1.15 },
+                new Item { ItemID = 2, Product = "Milk", Price = 1.15 },
+                new Item { ItemID = 2, Product = "Milk", Price = 1.15 },
+                new Item { ItemID = 2, Product = "Milk", Price = 1.15 },
+                new Item { ItemID = 2, Product = "Milk", Price = 1.15 },
+                new Item { ItemID = 2, Product = "Milk", Price = 1.15 }
+            };
+            foreach (var item in items)
+            {
+                basket.BasketItems.Add(new BasketItem { Item = item });
+            }
+            Assert.AreEqual(6.9, basket.Total);
+        }
+
+        // check offer logic still works for discounts with increased volumes
+        [TestMethod]
+        public void Discount_for_7_Milk()
+        {
+            var basket = new Basket { BasketItems = new List<BasketItem>() };
+            var items = new List<Item>
+            {
+                new Item { ItemID = 2, Product = "Milk", Price = 1.15 },
+                new Item { ItemID = 2, Product = "Milk", Price = 1.15 },
+                new Item { ItemID = 2, Product = "Milk", Price = 1.15 },
+                new Item { ItemID = 2, Product = "Milk", Price = 1.15 },
+                new Item { ItemID = 2, Product = "Milk", Price = 1.15 },
+                new Item { ItemID = 2, Product = "Milk", Price = 1.15 },
+                new Item { ItemID = 2, Product = "Milk", Price = 1.15 }
+            };
+            foreach (var item in items)
+            {
+                basket.BasketItems.Add(new BasketItem { Item = item });
+            }
+            Assert.AreEqual(1.15, basket.Discounts);
+        }
+
+        // check offer logic still works for totals with increased volumes
+        [TestMethod]
+        public void Total_for_8_Milk()
+        {
+            var basket = new Basket { BasketItems = new List<BasketItem>() };
+            var items = new List<Item>
+            {
+                new Item { ItemID = 2, Product = "Milk", Price = 1.15 },
+                new Item { ItemID = 2, Product = "Milk", Price = 1.15 },
+                new Item { ItemID = 2, Product = "Milk", Price = 1.15 },
+                new Item { ItemID = 2, Product = "Milk", Price = 1.15 },
+                new Item { ItemID = 2, Product = "Milk", Price = 1.15 },
+                new Item { ItemID = 2, Product = "Milk", Price = 1.15 },
+                new Item { ItemID = 2, Product = "Milk", Price = 1.15 },
+                new Item { ItemID = 2, Product = "Milk", Price = 1.15 }
+            };
+            foreach (var item in items)
+            {
+                basket.BasketItems.Add(new BasketItem { Item = item });
+            }
+            Assert.AreEqual(6.9, basket.Total);
+        }
+
+        // check offer logic still works for discounts with increased volumes
+        [TestMethod]
+        public void Discount_for_8_Milk()
+        {
+            var basket = new Basket { BasketItems = new List<BasketItem>() };
+            var items = new List<Item>
+            {
+                new Item { ItemID = 2, Product = "Milk", Price = 1.15 },
+                new Item { ItemID = 2, Product = "Milk", Price = 1.15 },
+                new Item { ItemID = 2, Product = "Milk", Price = 1.15 },
+                new Item { ItemID = 2, Product = "Milk", Price = 1.15 },
+                new Item { ItemID = 2, Product = "Milk", Price = 1.15 },
+                new Item { ItemID = 2, Product = "Milk", Price = 1.15 },
+                new Item { ItemID = 2, Product = "Milk", Price = 1.15 },
+                new Item { ItemID = 2, Product = "Milk", Price = 1.15 }
+            };
+            foreach (var item in items)
+            {
+                basket.BasketItems.Add(new BasketItem { Item = item });
+            }
+            Assert.AreEqual(2.3, basket.Discounts);
         }
 
         [TestMethod]
