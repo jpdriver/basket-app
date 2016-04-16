aaff7db4724beabfd9942cff00172454b3eca766
jpdriver <jpsdriver@gmail.com>
2016-04-16 20:18:21 +0100

    all tests passing

--- BasketApp.Tests/BL/BasketCalcuationsTest.cs
+++ BasketApp.Tests/BL/BasketCalcuationsTest.cs
@@ -1,6 +1,7 @@
 ﻿using BasketApp.Models;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace BasketApp.Tests.BL
 {
@@ -358,6 +359,68 @@ namespace BasketApp.Tests.BL
                 basket.BasketItems.Add(new BasketItem { Item = item });
             }
             Assert.AreEqual(9.00, basket.Total);
+        }
+
+        [TestMethod]
+        public void Discounts_for_2_Butter_1_Bread_8_Milk()
+        {
+            var basket = new Basket { BasketItems = new List<BasketItem>() };
+            var items = new List<Item>
+            {
+                new Item { ItemID = 1, Product = "Butter", Price = 0.8 },
+                new Item { ItemID = 1, Product = "Butter", Price = 0.8 },
+                new Item { ItemID = 2, Product = "Milk", Price = 1.15 },
+                new Item { ItemID = 2, Product = "Milk", Price = 1.15 },
+                new Item { ItemID = 2, Product = "Milk", Price = 1.15 },
+                new Item { ItemID = 2, Product = "Milk", Price = 1.15 },
+                new Item { ItemID = 2, Product = "Milk", Price = 1.15 },
+                new Item { ItemID = 2, Product = "Milk", Price = 1.15 },
+                new Item { ItemID = 2, Product = "Milk", Price = 1.15 },
+                new Item { ItemID = 2, Product = "Milk", Price = 1.15 },
+                new Item { ItemID = 3, Product = "Bread", Price = 1.0 }
+            };
+            foreach (var item in items)
+            {
+                basket.BasketItems.Add(new BasketItem { Item = item });
+            }
+            Assert.AreEqual(2.8, basket.Discounts);
+        }
+
+        // check we can pinpoint exactly which items discounts were given on
+        [TestMethod]
+        public void Individual_Discounts_for_2_Butter_1_Bread_8_Milk()
+        {
+            var basket = new Basket { BasketItems = new List<BasketItem>() };
+            var items = new List<Item>
+            {
+                new Item { ItemID = 1, Product = "Butter", Price = 0.8 },
+                new Item { ItemID = 1, Product = "Butter", Price = 0.8 },
+                new Item { ItemID = 2, Product = "Milk", Price = 1.15 },
+                new Item { ItemID = 2, Product = "Milk", Price = 1.15 },
+                new Item { ItemID = 2, Product = "Milk", Price = 1.15 },
+                new Item { ItemID = 2, Product = "Milk", Price = 1.15 },
+                new Item { ItemID = 2, Product = "Milk", Price = 1.15 },
+                new Item { ItemID = 2, Product = "Milk", Price = 1.15 },
+                new Item { ItemID = 2, Product = "Milk", Price = 1.15 },
+                new Item { ItemID = 2, Product = "Milk", Price = 1.15 },
+                new Item { ItemID = 3, Product = "Bread", Price = 1.0 }
+            };
+            foreach (var item in items)
+            {
+                basket.BasketItems.Add(new BasketItem { Item = item });
+            }
+            Assert.AreEqual(2.8, basket.Discounts);
+            Assert.IsTrue(basket.BasketItems.ElementAt(0).Discount == 0);
+            Assert.IsTrue(basket.BasketItems.ElementAt(1).Discount == 0);
+            Assert.IsTrue(basket.BasketItems.ElementAt(2).Discount == 0);
+            Assert.IsTrue(basket.BasketItems.ElementAt(3).Discount == 0);
+            Assert.IsTrue(basket.BasketItems.ElementAt(4).Discount == 0);
+            Assert.IsTrue(basket.BasketItems.ElementAt(5).Discount == 1.15); // 4th Milk
+            Assert.IsTrue(basket.BasketItems.ElementAt(6).Discount == 0);
+            Assert.IsTrue(basket.BasketItems.ElementAt(7).Discount == 0);
+            Assert.IsTrue(basket.BasketItems.ElementAt(8).Discount == 0);
+            Assert.IsTrue(basket.BasketItems.ElementAt(9).Discount == 1.15); // 8th Milk
+            Assert.IsTrue(basket.BasketItems.ElementAt(10).Discount == 0.5); // Bread w/ 2 Butter
         }
     }
 }
